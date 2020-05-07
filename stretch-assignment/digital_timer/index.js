42a733341b672c662b63233334b21794a6c7416d
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2020-05-06 23:25:12 -0400

    All stretch goals done

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -3,44 +3,81 @@ function StartTimer () {
     let intervalTime = 10 // ms
     let timeLimiter = 10 //in seconds
     let miliSecondCounter = 0
-
+    
+    
     // timer elements
+    // color initialization
     let secondTensElement = document.getElementById("secondTens")
+    secondTensElement.style.color = "black"
     let secondOnesElement = document.getElementById("secondOnes")
+    secondOnesElement.style.color = "black"
     let colonElement = document.getElementById("colon")
     let msHundredsElement = document.getElementById("msHundreds")
+    msHundredsElement.style.color = "black"
     let msTensElement = document.getElementById("msTens")
+    msTensElement.style.color = "black"
 
     // Timer Variables
     let secondTens = 0
     let secondOnes = 0
-    let colon = 0
     let msHundreds = 0
 
-   let myInterval = setInterval(function() {
+    // button Element
+    let buttonElement = document.querySelector("button")
+
+
+
+
+    if(buttonElement.textContent == "Start Timer") {
+        buttonElement.disabled = true;
+        let myInterval = setInterval(function() {
         
-        secondTensElement.textContent = secondTens
-        secondOnesElement.textContent = secondOnes
-        msHundredsElement.textContent = msHundreds
-        msTensElement.textContent = miliSecondCounter - 1
-        if(secondTens == 1) {
-            clearInterval(myInterval)
-        }
-        if(miliSecondCounter == 10) {
-            miliSecondCounter = 0
-            msHundreds ++
-        }
-        if(msHundreds == 10) {
-            msHundreds = 0
-            secondOnes ++
-        }
-        if(secondOnes == 10) {
-            secondOnes = 0
-            secondTens ++
-        }
-        
-        miliSecondCounter ++;
-        
-        
-    }, intervalTime)
+            secondTensElement.textContent = secondTens
+            secondOnesElement.textContent = secondOnes
+            msHundredsElement.textContent = msHundreds
+            msTensElement.textContent = miliSecondCounter - 1
+            if(secondTens == 1) {
+                clearInterval(myInterval)
+                secondTensElement.style.color = "red"
+                secondOnesElement.style.color = "red"
+                msHundredsElement.style.color = "red"
+                msTensElement.style.color = "red"
+                buttonElement.textContent = "Reset Timer"
+                buttonElement.disabled = false
+            }
+            if(miliSecondCounter == 10) {
+                miliSecondCounter = 0
+                msHundreds ++
+            }
+    
+            if(msHundreds == 5) {
+                colonElement.style.color = 'transparent'
+            } else if(msHundreds == 0) {
+                colonElement.style.color = 'black'
+            }
+    
+            if(msHundreds == 10) {
+                msHundreds = 0
+                secondOnes ++
+            }
+            if(secondOnes == 10) {
+                secondOnes = 0
+                secondTens ++
+            }
+            
+            miliSecondCounter ++;
+            
+            
+        }, intervalTime)
+    } else if(buttonElement.textContent == "Reset Timer") {
+        secondTensElement.textContent = 0
+        secondOnesElement.textContent = 0
+        msHundredsElement.textContent = 0
+        msTensElement.textContent = 0
+        buttonElement.textContent = "Start Timer"
+    }
+    
+    
+
+    
 }
