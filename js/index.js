edb36b043d3953bfc7b03f81dc106919332c5f82
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2020-05-06 21:41:28 -0400

    Contact, footer and main Content done

--- js/index.js
+++ js/index.js
@@ -42,13 +42,48 @@ let logo = document.getElementById("logo-img")
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 let ctaImg = document.getElementById("cta-img")
-ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+ctaImg.setAttribute('src', siteContent['cta']['img-src'])
 
 let middleImg = document.getElementById("middle-img")
-middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src'])
 
 // Nav Items
 let navItems = document.querySelectorAll("nav a")
 navItems.forEach((el, index) => {
-  el.textContent = siteContent["nav"][`nav-item-${(index+1)}`]
+  el.textContent = siteContent['nav'][`nav-item-${(index+1)}`]
+});
+
+// CTA Content
+let ctaHeading1 = document.querySelector('.cta h1')
+ctaHeading1.textContent = siteContent['cta']['h1']
+
+let ctaButton = document.querySelector('.cta button')
+ctaButton.textContent = siteContent['cta']['button']
+
+// Contact Content
+let contactHeading4 = document.querySelector('.contact h4')
+contactHeading4.textContent = siteContent['contact']['contact-h4']
+
+let contactAddress = document.querySelectorAll('.contact p')
+let contactContentOrder = ['address', 'phone','email']
+contactAddress.forEach((el, index) => {
+  el.textContent = siteContent['contact'][contactContentOrder[index]]
+});
+
+
+// footer
+let footerCopy = document.querySelector('footer p')
+footerCopy.textContent = siteContent['footer']['copyright']
+
+// Main Content
+let mainHeading4 = document.querySelectorAll('.text-content h4')
+let mainSectionContent = document.querySelectorAll('.text-content p')
+
+mainContentOrder = ['features', 'about', 'services', 'product', 'vision']
+
+mainHeading4.forEach((el, index) => {
+  el.textContent = siteContent['main-content'][`${mainContentOrder[index]}-h4`]
+});
+mainSectionContent.forEach((el, index) => {
+  el.textContent = siteContent['main-content'][`${mainContentOrder[index]}-content`]
 });
