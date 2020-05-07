365ce2aec6fbcf0c9bed05d6c089f3497afe47ee
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2020-05-06 21:16:15 -0400

    imgs and neav links done

--- js/index.js
+++ js/index.js
@@ -38,5 +38,17 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
+let logo = document.getElementById("logo-img")
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let ctaImg = document.getElementById("cta-img")
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+let middleImg = document.getElementById("middle-img")
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+// Nav Items
+let navItems = document.querySelectorAll("nav a")
+navItems.forEach((el, index) => {
+  el.textContent = siteContent["nav"][`nav-item-${(index+1)}`]
+});
